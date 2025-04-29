dee4895f8c9475a49d462ba399897d71507c69aa
abdallh khattab <freelancerabdallah@gmail.com>
2025-04-29 14:38:04 +0300

    added footer

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,14 @@
 import DiscoverOurOfficeLocations from "../../components/DiscoverOurOfficeLocations/DiscoverOurOfficeLocations"
 import GetInTouchWithEstateinHero from "../../components/GetInTouchWithEstateinHero/GetInTouchWithEstateinHero"
 import LetsConnect from "../../components/LetsConnect/LetsConnect"
-
+import FinnalFooter from "../../components/FinnalFooter/FinnalFooter"
 const ContactUs = () => {
   return (
     <div className="ContactUs bg-gray8 text-white ">
       <GetInTouchWithEstateinHero />
       <LetsConnect />
       <DiscoverOurOfficeLocations />
+      <FinnalFooter />
     </div>
   )
 }
